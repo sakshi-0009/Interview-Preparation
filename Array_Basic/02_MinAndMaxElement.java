5eb494e21ba793c433afe3a73723595203360f23
sakshi-0009 <sakshigome1809@gmail.com>
2023-09-21 19:28:52 +0530

    Minimum and Maximum element in an Array

--- Array_Basic/02_MinAndMaxElement.java
+++ Array_Basic/02_MinAndMaxElement.java
@@ -2,7 +2,7 @@ package Array_Basic;
 
 import java.io.*;
 
-public class MinAndMaxElement {
+class MinAndMaxElement {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
