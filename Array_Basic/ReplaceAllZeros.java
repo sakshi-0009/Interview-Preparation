b6557d4c36e7e55499a68040cdfaedd7a5cf2143
sakshi-0009 <sakshigome1809@gmail.com>
2023-09-21 19:19:28 +0530

    Replace all 0's with 5

--- Array_Basic/ReplaceAllZeros.java
+++ Array_Basic/ReplaceAllZeros.java
@@ -1,5 +1,20 @@
 package Array_Basic;
 
+import java.io.*;
+
 public class ReplaceAllZeros {
 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter Number : ");
+		int N = Integer.parseInt(br.readLine());
+		
+		String strNum = String.valueOf(N);
+
+		strNum = strNum.replaceAll("0", "5");
+
+        int newNum = Integer.parseInt(strNum);
+
+        System.out.println("Number after replacing 0's with 5 : "+newNum);
+    }
 }
