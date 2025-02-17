9862e236c88357355227e42a4f79232eedd3eca8
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2025-02-17 11:34:55 +0530

    Even Numbers

--- Array_01/EvenNumber.java
+++ Array_01/EvenNumber.java
@@ -15,7 +15,6 @@ public class EvenNumber {
 			}
 		}
 	}
-	
 	public static void main(String[] args)throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
